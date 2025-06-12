59b1d75bd4ae364f3e153312598c6924486b7e66
Basharfrancis <bashar.fransis@rafflebox.ca>
2025-06-11 20:52:24 -0600

    New update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import githubRouter from "./routes/github";
 import dotenv from "dotenv";
+
+import githubRoutes from "./routes/github";
 
 dotenv.config();
 const app = express();
 
-app.use("/api/github", githubRouter);
+app.use("/api/github", githubRoutes);
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
--- src/routes/github.ts
+++ src/routes/github.ts
@@ -2,9 +2,18 @@ import express from "express";
 import { fetchCommits } from "../services/githubService";
 
 const router = express.Router();
+
 router.get("/commits", async (req, res) => {
-  const data = await fetchCommits("Basharfrancis", "devtrack");
-  res.json(data);
+  const owner = (req.query.owner as string) || "Basharfrancis";
+  const repo = (req.query.repo as string) || "devtrack";
+
+  try {
+    const commits = await fetchCommits(owner, repo);
+    res.json(commits);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error fetching commits" });
+  }
 });
 
 export default router;
